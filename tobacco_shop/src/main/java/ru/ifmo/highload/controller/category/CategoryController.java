a57f43c28c62e2f98297e30067ad5c9168b84716
d.lyanguzov <d.lyanguzov@geoscan.ru>
2025-10-27 22:29:04 +0300

    feat: Implement controllers
    Add implementation of category, discount, order, price and product controllers

---
+++ tobacco_shop/src/main/java/ru/ifmo/highload/controller/category/CategoryController.java
@@ -0,0 +1,40 @@
+package ru.ifmo.highload.controller.category;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.ifmo.highload.api.CategoryService;
+import ru.ifmo.highload.dto.category.CategoryCreateRequest;
+import ru.ifmo.highload.dto.category.CategoryResponse;
+import ru.ifmo.highload.dto.category.CategoryUpdateRequest;
+
+@RestController
+@RequestMapping("/api/category")
+@RequiredArgsConstructor
+public class CategoryController implements CategoryApi {
+
+    private final CategoryService categoryService;
+
+    @Override
+    public ResponseEntity<Page<CategoryResponse>> getCategories(Pageable pageable) {
+        return ResponseEntity.ok(categoryService.getAllCategories(pageable));
+    }
+
+    @Override
+    public ResponseEntity<CategoryResponse> createCategory(CategoryCreateRequest request) {
+        return ResponseEntity.ok(categoryService.createCategory(request));
+    }
+
+    @Override
+    public ResponseEntity<CategoryResponse> updateCategory(Long id, CategoryUpdateRequest request) {
+        return ResponseEntity.ok(categoryService.updateCategory(id, request));
+    }
+
+    @Override
+    public ResponseEntity<Void> deleteCategory(Long id) {
+        categoryService.deleteCategory(id);
+        return ResponseEntity.ok().build();
+    }
+}
