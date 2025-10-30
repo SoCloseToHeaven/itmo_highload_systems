03e3d12787670350fb4f26fa179796326ce6cec9
d.lyanguzov <d.lyanguzov@geoscan.ru>
2025-10-30 18:41:44 +0300

    feat: Configure testcontainers configuration, add first integration test
    Add integration test of OrderService

--- tobacco_shop/src/test/java/ru/ifmo/highload/config/TestcontainersConfiguration.java
+++ tobacco_shop/src/test/java/ru/ifmo/highload/config/TestcontainersConfiguration.java
@@ -1,18 +1,33 @@
 package ru.ifmo.highload.config;
 
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
-import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.utility.DockerImageName;
+import org.testcontainers.junit.jupiter.Container;
 
 @TestConfiguration(proxyBeanMethods = false)
-public class TestcontainersConfiguration {
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public abstract class TestcontainersConfiguration {
 
-    @Bean
-    @ServiceConnection
-    PostgreSQLContainer<?> postgresContainer() {
-        return new PostgreSQLContainer<>(DockerImageName.parse("postgres:latest"));
+    @Container
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:15-alpine")
+            .withDatabaseName("testdb")
+            .withUsername("test")
+            .withPassword("test");
+
+    static {
+        // Принудительно запускаем контейнер до любых операций
+        postgres.start();
+    }
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+        registry.add("spring.jpa.hibernate.ddl-auto", () -> "validate");
     }
 
 }
