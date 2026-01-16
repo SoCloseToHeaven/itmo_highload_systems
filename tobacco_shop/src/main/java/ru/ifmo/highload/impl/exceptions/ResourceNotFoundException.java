85e5fa4e6094ca75d1dfb5fa3eabd5cb9fed6c66
d.lyanguzov <d.lyanguzov@geoscan.ru>
2026-01-16 10:50:04 +0300

    legacy: Remove legacy lab1 (tobacco_shop) modules.

--- tobacco_shop/src/main/java/ru/ifmo/highload/impl/exceptions/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package ru.ifmo.highload.impl.exceptions;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
